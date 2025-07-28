ba83063d17d2c60e4d5e38fb678b808d10cbea38
sosopanda <mcamarasantacreu@gmail.com>
2025-07-28 13:10:04 +0200

    Added code to js so now you can play a round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
 
     let choice = "none";
@@ -13,6 +16,50 @@ function getComputerChoice(){
 
     console.log("The computer choses " + choice);
     return choice
+
 }
 
-getComputerChoice();
+function getHumanChoice(){
+
+    let choice = prompt("Please select Rock, Paper or Scissors");
+    console.log("You chose " + choice);
+    return choice;
+
+}
+
+function playRound(){
+
+    let human = getHumanChoice();
+    let computer = getComputerChoice();
+
+    if(String(human).toLowerCase() == String(computer)){
+        console.log("Tie!");
+    } else if(String(human).toLowerCase() == "rock") {
+        if(String(computer) == "paper"){
+            computerScore++;
+            console.log("You Lose!");
+        }else{
+            humanScore++;
+            console.log("You Win!");
+        }
+    } else if(String(human).toLowerCase() == "paper") {
+        if(String(computer) == "scissors"){
+            computerScore++;
+            console.log("You Lose!");
+        }else{
+            humanScore++;
+            console.log("You Win!");
+        }
+    } else if(String(human).toLowerCase() == "scissors") {
+        if(String(computer) == "rock"){
+            computerScore++;
+            console.log("You Lose!");
+        }else{
+            humanScore++;
+            console.log("You Win!");
+        }
+    }
+
+}
+
+playRound();
